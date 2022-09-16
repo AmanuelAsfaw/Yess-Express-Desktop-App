119bbc6c15c9b2d9eb8081b6dadbc06952455734
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-09-16 11:31:18 +0300

    Add Print View components

--- Yess Express - Desktop App/PrintView.cs
+++ Yess Express - Desktop App/PrintView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Text;
 using System.Windows.Forms;
 
@@ -12,6 +13,33 @@ namespace Yess_Express___Desktop_App
     {
         public string senderName, senderPhone, senderCompanyNameAndAddress, senderSendDate, yesExpressReceivedPerson, yesExpressReceivedDateTime, descriptionOfGoods, killo, gram, shipmentLength, shipmentWidth, shipmentHeight, shipmentVolume,
                 consigneePerson, consigneePhone, consigneeCompanyNameAndAddress, receivedDateTime, receiverName, receiverServiceType, amountReceived, specialInstruction, paymentMethod, tracking_no;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Print(this.panelPrint);
+        }
+        private Bitmap memoryimg;
+        private void Print(Panel pnl)
+        {
+            PrinterSettings printerSettings = new PrinterSettings();
+            panelPrint = pnl;
+            getPrintArea(pnl);
+            printDocument1.DefaultPageSettings.Landscape = true;
+            printPreviewDialog1.Document = printDocument1;
+            printDocument1.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            printPreviewDialog1.ShowDialog();
+        }
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Rectangle pagearea = e.PageBounds;
+            e.Graphics.DrawImage(memoryimg, (pagearea.Width / 2) - (this.panelPrint.Width / 2), this.panelPrint.Location.Y);
+        }
+        private void getPrintArea(Panel pnl)
+        {
+            memoryimg = new Bitmap(pnl.Width, pnl.Height);
+            pnl.DrawToBitmap(memoryimg, new Rectangle(0, 0, pnl.Width, pnl.Height));
+        }
+
         public PrintView()
         {
             InitializeComponent();
@@ -23,7 +51,7 @@ namespace Yess_Express___Desktop_App
 
         private void PrintView_Load(object sender, EventArgs e)
         {
-            labelTrackingNo.Text = tracking_no;
+            /*labelTrackingNo.Text = tracking_no;
             labelSenderName.Text = senderName;
             labelSenderPhone.Text = senderPhone;
             labelShipperCompnyNameAndAddress.Text = senderCompanyNameAndAddress;
@@ -45,7 +73,7 @@ namespace Yess_Express___Desktop_App
             labelServiceType.Text = receiverServiceType;
             labelAmountReceived.Text = amountReceived;
             labelPaymentMethod.Text = paymentMethod;
-            labelSpecialInstructions.Text = specialInstruction;
+            labelSpecialInstructions.Text = specialInstruction;*/
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
